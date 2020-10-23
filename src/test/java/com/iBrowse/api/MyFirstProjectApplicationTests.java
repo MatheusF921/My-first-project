6a1f69c0e7c260d508e38f1c4e66cbee8ef7d510
MatheusF921 <matheus.s.freire1998@icloud.com>
2020-10-23 09:30:10 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/iBrowse/api/MyFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iBrowse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
